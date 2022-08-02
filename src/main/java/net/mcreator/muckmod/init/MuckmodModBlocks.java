9e7ee7c7d2d3785a72abe77c0ff134a4b13a9ff2
PhillayDeveloping <lindb@192.168.1.43>
2022-08-02 22:09:13 +0200

    Added Muck Dirt And Grass Block

--- src/main/java/net/mcreator/muckmod/init/MuckmodModBlocks.java
+++ src/main/java/net/mcreator/muckmod/init/MuckmodModBlocks.java
@@ -21,6 +21,8 @@ import net.mcreator.muckmod.block.MuckPackFenceGateBlock;
 import net.mcreator.muckmod.block.MuckPackFenceBlock;
 import net.mcreator.muckmod.block.MuckPackButtonBlock;
 import net.mcreator.muckmod.block.MuckOreBlock;
+import net.mcreator.muckmod.block.MuckGrassBlockBlock;
+import net.mcreator.muckmod.block.MuckDirtBlockBlock;
 import net.mcreator.muckmod.block.MuckBlockBlock;
 import net.mcreator.muckmod.MuckmodMod;
 
@@ -39,4 +41,6 @@ public class MuckmodModBlocks {
 	public static final RegistryObject<Block> MUCK_PACK_PRESSURE_PLATE = REGISTRY.register("muck_pack_pressure_plate",
 			() -> new MuckPackPressurePlateBlock());
 	public static final RegistryObject<Block> MUCK_PACK_BUTTON = REGISTRY.register("muck_pack_button", () -> new MuckPackButtonBlock());
+	public static final RegistryObject<Block> MUCK_DIRT_BLOCK = REGISTRY.register("muck_dirt_block", () -> new MuckDirtBlockBlock());
+	public static final RegistryObject<Block> MUCK_GRASS_BLOCK = REGISTRY.register("muck_grass_block", () -> new MuckGrassBlockBlock());
 }
--- src/main/java/net/mcreator/muckmod/init/MuckmodModItems.java
+++ src/main/java/net/mcreator/muckmod/init/MuckmodModItems.java
@@ -27,23 +27,9 @@ import net.mcreator.muckmod.MuckmodMod;
 
 public class MuckmodModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MuckmodMod.MODID);
-	public static final RegistryObject<Item> MUCK_MUSIC = REGISTRY.register("muck_music", () -> new MuckMusicItem());
 	public static final RegistryObject<Item> MUCK_INGOT = REGISTRY.register("muck_ingot", () -> new MuckIngotItem());
 	public static final RegistryObject<Item> MUCK_ORE = block(MuckmodModBlocks.MUCK_ORE, MuckmodModTabs.TAB_MUCK_MOD);
 	public static final RegistryObject<Item> MUCK_BLOCK = block(MuckmodModBlocks.MUCK_BLOCK, MuckmodModTabs.TAB_MUCK_MOD);
-	public static final RegistryObject<Item> MUCK_PICKAXE = REGISTRY.register("muck_pickaxe", () -> new MuckPickaxeItem());
-	public static final RegistryObject<Item> MUCK_AXE = REGISTRY.register("muck_axe", () -> new MuckAxeItem());
-	public static final RegistryObject<Item> MUCK_SWORD = REGISTRY.register("muck_sword", () -> new MuckSwordItem());
-	public static final RegistryObject<Item> MUCK_SHOVEL = REGISTRY.register("muck_shovel", () -> new MuckShovelItem());
-	public static final RegistryObject<Item> MUCK_HOE = REGISTRY.register("muck_hoe", () -> new MuckHoeItem());
-	public static final RegistryObject<Item> MUCK_ARMOR_HELMET = REGISTRY.register("muck_armor_helmet", () -> new MuckArmorItem.Helmet());
-	public static final RegistryObject<Item> MUCK_ARMOR_CHESTPLATE = REGISTRY.register("muck_armor_chestplate", () -> new MuckArmorItem.Chestplate());
-	public static final RegistryObject<Item> MUCK_ARMOR_LEGGINGS = REGISTRY.register("muck_armor_leggings", () -> new MuckArmorItem.Leggings());
-	public static final RegistryObject<Item> MUCK_ARMOR_BOOTS = REGISTRY.register("muck_armor_boots", () -> new MuckArmorItem.Boots());
-	public static final RegistryObject<Item> MOBMUCK = REGISTRY.register("mobmuck_spawn_egg",
-			() -> new ForgeSpawnEggItem(MuckmodModEntities.MOBMUCK, -1, -1, new Item.Properties().tab(MuckmodModTabs.TAB_MUCK_MOD)));
-	public static final RegistryObject<Item> KING_MUCK = REGISTRY.register("king_muck_spawn_egg",
-			() -> new ForgeSpawnEggItem(MuckmodModEntities.KING_MUCK, -16724992, -1, new Item.Properties().tab(MuckmodModTabs.TAB_MUCK_MOD)));
 	public static final RegistryObject<Item> MUCK_PACK_WOOD = block(MuckmodModBlocks.MUCK_PACK_WOOD, MuckmodModTabs.TAB_MUCK_MOD);
 	public static final RegistryObject<Item> MUCK_PACK_LOG = block(MuckmodModBlocks.MUCK_PACK_LOG, MuckmodModTabs.TAB_MUCK_MOD);
 	public static final RegistryObject<Item> MUCK_PACK_PLANKS = block(MuckmodModBlocks.MUCK_PACK_PLANKS, MuckmodModTabs.TAB_MUCK_MOD);
@@ -54,9 +40,25 @@ public class MuckmodModItems {
 	public static final RegistryObject<Item> MUCK_PACK_FENCE_GATE = block(MuckmodModBlocks.MUCK_PACK_FENCE_GATE, MuckmodModTabs.TAB_MUCK_MOD);
 	public static final RegistryObject<Item> MUCK_PACK_PRESSURE_PLATE = block(MuckmodModBlocks.MUCK_PACK_PRESSURE_PLATE, MuckmodModTabs.TAB_MUCK_MOD);
 	public static final RegistryObject<Item> MUCK_PACK_BUTTON = block(MuckmodModBlocks.MUCK_PACK_BUTTON, MuckmodModTabs.TAB_MUCK_MOD);
+	public static final RegistryObject<Item> MUCK_SWORD = REGISTRY.register("muck_sword", () -> new MuckSwordItem());
+	public static final RegistryObject<Item> MUCK_PICKAXE = REGISTRY.register("muck_pickaxe", () -> new MuckPickaxeItem());
+	public static final RegistryObject<Item> MUCK_SHOVEL = REGISTRY.register("muck_shovel", () -> new MuckShovelItem());
+	public static final RegistryObject<Item> MUCK_AXE = REGISTRY.register("muck_axe", () -> new MuckAxeItem());
+	public static final RegistryObject<Item> MUCK_HOE = REGISTRY.register("muck_hoe", () -> new MuckHoeItem());
+	public static final RegistryObject<Item> CROWN_HELMET = REGISTRY.register("crown_helmet", () -> new CrownItem.Helmet());
+	public static final RegistryObject<Item> MUCK_MUSIC = REGISTRY.register("muck_music", () -> new MuckMusicItem());
+	public static final RegistryObject<Item> MOBMUCK = REGISTRY.register("mobmuck_spawn_egg",
+			() -> new ForgeSpawnEggItem(MuckmodModEntities.MOBMUCK, -1, -1, new Item.Properties().tab(MuckmodModTabs.TAB_MUCK_MOD)));
+	public static final RegistryObject<Item> KING_MUCK = REGISTRY.register("king_muck_spawn_egg",
+			() -> new ForgeSpawnEggItem(MuckmodModEntities.KING_MUCK, -16724992, -1, new Item.Properties().tab(MuckmodModTabs.TAB_MUCK_MOD)));
 	public static final RegistryObject<Item> KING_GOLEM = REGISTRY.register("king_golem_spawn_egg",
 			() -> new ForgeSpawnEggItem(MuckmodModEntities.KING_GOLEM, -1, -1, new Item.Properties().tab(MuckmodModTabs.TAB_MUCK_MOD)));
-	public static final RegistryObject<Item> CROWN_HELMET = REGISTRY.register("crown_helmet", () -> new CrownItem.Helmet());
+	public static final RegistryObject<Item> MUCK_ARMOR_HELMET = REGISTRY.register("muck_armor_helmet", () -> new MuckArmorItem.Helmet());
+	public static final RegistryObject<Item> MUCK_ARMOR_CHESTPLATE = REGISTRY.register("muck_armor_chestplate", () -> new MuckArmorItem.Chestplate());
+	public static final RegistryObject<Item> MUCK_ARMOR_LEGGINGS = REGISTRY.register("muck_armor_leggings", () -> new MuckArmorItem.Leggings());
+	public static final RegistryObject<Item> MUCK_ARMOR_BOOTS = REGISTRY.register("muck_armor_boots", () -> new MuckArmorItem.Boots());
+	public static final RegistryObject<Item> MUCK_DIRT_BLOCK = block(MuckmodModBlocks.MUCK_DIRT_BLOCK, MuckmodModTabs.TAB_MUCK_MOD);
+	public static final RegistryObject<Item> MUCK_GRASS_BLOCK = block(MuckmodModBlocks.MUCK_GRASS_BLOCK, MuckmodModTabs.TAB_MUCK_MOD);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
